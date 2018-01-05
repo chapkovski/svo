69489c93ca70509855be1817da2961384b14fc43
chapkovski <chapkovski@gmail.com>
2018-01-05 21:46:01 +0300

    asdf

--- settings.py
+++ settings.py
@@ -64,7 +64,7 @@ USE_POINTS = True
 LANGUAGE_CODE = 'en'
 
 # if an app is included in SESSION_CONFIGS, you don't need to list it here
-INSTALLED_APPS = ['otree']
+INSTALLED_APPS = ['otree', 'otree_export_utils']
 
 # SENTRY_DSN = ''
 
